e3330b6857adeaed03fe4aec164ecbf1002aeb81
RoxJade <roxannejade88@icloud.com>
2021-03-26 12:24:34 +0000

    update a typo error in env.py MONGO URI connection

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
